b5abc176d24b3d93676f905d6f555a36aba7e437
OrdiPJ <paulin.juhen@outlook.com>
2021-07-27 19:07:54 +0200

    La carte peut se retourner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log("Chargement du JavaScript");
+
+function flip(teventhis){
+    let element = event.currentTarget;
+    if (element.className === "card"){
+        if(element.style.transform == "rotateY(180deg)"){
+            element.style.transform = "rotateY(0deg)";
+        }
+        else {
+            element.style.transform = "rotateY(180deg)"
+        }
+    }
+}
