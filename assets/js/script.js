05f9972c11a0f6f8a7c344285993b77e99e95468
OrdiPJ <paulin.juhen@outlook.com>
2021-07-31 12:05:49 +0200

    Version finale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-console.log("Chargement du JavaScript");
 
-function flip(teventhis){
+// recuperer le bouton avec l'id "btn"
+let btn = document.getElementById('btn')
+btn.addEventListener('click', shuffle)
+
+let redcard = 0;
+
+shuffle()
+
+function shuffle() {
+    let allcards = document.getElementsByClassName("card-img");
+    allcards[redcard].setAttribute('src', "./assets/img/black.png")
+    let random = Math.floor(Math.random() * allcards.length); // un nombre au hasard
+    let card = allcards[random];
+    card.setAttribute('src', "./assets/img/red.png");
+    redcard = random
+}
+
+function flip(event){
     let element = event.currentTarget;
     if (element.className === "card"){
         if(element.style.transform == "rotateY(180deg)"){
             element.style.transform = "rotateY(0deg)";
         }
-        else {
-            element.style.transform = "rotateY(180deg)"
+        else{
+            element.style.transform = "rotateY(180deg)";
         }
     }
 }
